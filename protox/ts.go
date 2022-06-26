1e423cf7c7ad370bad4f259bb008c9ccccb5ead4
wener <wenermail@gmail.com>
2022-06-26 16:12:37 +0800

    feat(protox): add protox

---
+++ protox/ts.go
@@ -0,0 +1,41 @@
+package protox
+
+import (
+	"time"
+
+	"google.golang.org/protobuf/types/known/durationpb"
+
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func UnixToTimestamp(t int64) *timestamppb.Timestamp {
+	if t == 0 {
+		return nil
+	}
+	return &timestamppb.Timestamp{
+		Seconds: t,
+	}
+}
+
+func UnixMilliToTimestamp(t int64) *timestamppb.Timestamp {
+	if t == 0 {
+		return nil
+	}
+	return ToTimestamp(time.UnixMilli(t))
+}
+
+func ToTimestamp(t time.Time) *timestamppb.Timestamp {
+	if t.IsZero() {
+		return nil
+	}
+	return timestamppb.New(t)
+}
+
+func PtrToTimestamp(t *time.Time) *timestamppb.Timestamp {
+	if t == nil {
+		return nil
+	}
+	return ToTimestamp(*t)
+}
+
+var ToDuration = durationpb.New
