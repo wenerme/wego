3caf5a7f5d2f2c09fe460fc78f924d1e8137d875
wener <wenermail@gmail.com>
2022-06-29 21:30:29 +0800

    feat(contextx): more helper funcs

--- contextx/key.go
+++ contextx/key.go
@@ -7,18 +7,42 @@ import (
 	"reflect"
 )
 
+// Key is a context key
 type Key[T any] struct {
 	name string
 	x    [8]byte
 }
 
-func (key Key[T]) FromContext(ctx context.Context) (T, bool) {
-	o, ok := ctx.Value(key).(T)
+// FromContext returns the value stored in the context for the given key.
+func (key Key[T]) FromContext(ctx context.Context) (o T, ok bool) {
+	if ctx == nil {
+		ok = false
+		return
+	}
+	o, ok = ctx.Value(key).(T)
 	return o, ok
 }
 
+// NewContext returns a new context with the given key and value set.
 func (key Key[T]) NewContext(ctx context.Context, val T) context.Context {
 	return context.WithValue(ctx, key, val)
+}
+
+// WithDefaultProvider is a helper function to create a new key with a default value provided by a provider
+func (key Key[T]) WithDefaultProvider(ctx context.Context, provider func(context.Context) (context.Context, T)) context.Context {
+	if key.Exists(ctx) {
+		return ctx
+	}
+	next, val := provider(ctx)
+	return key.NewContext(next, val)
+}
+
+// WithDefaultValue is a helper function to create a new key with a default value
+func (key Key[T]) WithDefaultValue(ctx context.Context, val T) context.Context {
+	if key.Exists(ctx) {
+		return ctx
+	}
+	return key.NewContext(ctx, val)
 }
 
 func (key Key[T]) String() string {
@@ -29,29 +53,43 @@ func (key Key[T]) String() string {
 	return fmt.Sprintf("contextx.Key(%s%s)", reflect.TypeOf(new(T)).Elem().String(), name)
 }
 
+// Get returns the value stored in the context for the given key.
 func (key Key[T]) Get(ctx context.Context) T {
-	o, _ := ctx.Value(key).(T)
+	o, _ := key.FromContext(ctx)
 	return o
 }
 
+// GetWithDefault is a helper function to get a value from the context with a default value
+func (key Key[T]) GetWithDefault(ctx context.Context, def T) T {
+	v, found := key.FromContext(ctx)
+	if !found {
+		return def
+	}
+	return v
+}
+
+// Exists return true if the context contains a value for the given key.
 func (key Key[T]) Exists(ctx context.Context) bool {
-	_, ok := ctx.Value(key).(T)
+	_, ok := key.FromContext(ctx)
 	return ok
 }
 
+// Must return the value stored in the context for the given key.
 func (key Key[T]) Must(ctx context.Context) T {
-	o, ok := ctx.Value(key).(T)
+	o, ok := key.FromContext(ctx)
 	if !ok {
-		panic(fmt.Errorf("%s not found in context", key.String()))
+		panic(fmt.Errorf("%s: not found in context", key.String()))
 	}
 	return o
 }
 
+// KeyOptions is a struct to configure a key
 type KeyOptions struct {
-	Private bool
-	Name    string
+	Name    string // the name of the key
+	Private bool   // if true, the key is private and should not be shared
 }
 
+// NewKey returns a new Key
 func NewKey[T any](o *KeyOptions) *Key[T] {
 	if o == nil {
 		o = &KeyOptions{}
--- contextx/key_test.go
+++ contextx/key_test.go
@@ -17,15 +17,31 @@ func TestKey(t *testing.T) {
 	assert.False(t, k.Exists(context.Background()))
 	assert.True(t, k.Exists(ctx))
 	assert.Equal(t, "contextx.Key(int)", k.String())
-	assert.Equal(t, "contextx.Key(int@X)", Key[int]{name: "X"}.String())
+	assert.Equal(t, "contextx.Key(int@X)", NewKey[int](&KeyOptions{Name: "X"}).String())
 	b := k
-	assert.Equal(t, b, k)
-	assert.Equal(t, b.Must(ctx), 1)
-	assert.Equal(t, Key[int]{}.Must(ctx), 1)
-	assert.Equal(t, (&Key[int]{}).Must(ctx), 1)
-	assert.Equal(t, Key[int]{name: "X"}.Get(ctx), 0)
+	c := NewKey[int](nil)
 
-	k2 := NewKey[int](&KeyOptions{Private: true})
-	assert.Equal(t, k2.Get(ctx), 0)
-	assert.NotEqual(t, k2, k)
+	assert.Equal(t, b, k)
+	assert.Equal(t, k, c)
+	assert.Equal(t, b.Must(ctx), 1)
+	assert.Equal(t, c.Must(ctx), 1)
+	assert.Equal(t, (&Key[int]{name: "X"}).Get(ctx), 0)
+	{
+		k2 := NewKey[int](&KeyOptions{Private: true})
+		k3 := NewKey[int](&KeyOptions{Private: true})
+
+		assert.NotEqual(t, k2, k3)
+		assert.Equal(t, k2.Get(ctx), 0)
+		assert.Equal(t, k2.Get(nil), 0)
+		assert.Equal(t, k2.GetWithDefault(nil, 10), 10)
+		assert.NotEqual(t, k2, k)
+
+		ctx = k3.WithDefaultProvider(ctx, func(ctx context.Context) (context.Context, int) {
+			ctx = k2.WithDefaultValue(ctx, 2)
+			return ctx, 3
+		})
+
+		assert.Equal(t, k2.Get(ctx), 2)
+		assert.Equal(t, k3.Get(ctx), 3)
+	}
 }
